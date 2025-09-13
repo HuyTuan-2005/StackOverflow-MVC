d997984c4d8744238c101c1b88b42e2df28c50a5
HuyTuan <HuyTuan.learn@gmail.com>
2025-09-13 12:29:19 +0700

    Test

--- StackOverflow/Controllers/AdminController.cs
+++ StackOverflow/Controllers/AdminController.cs
@@ -1,5 +1,8 @@
 ﻿using System.IO;
 using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Web.Mvc;
 using StackOverflow.Models;
 
@@ -7,28 +10,62 @@ namespace StackOverflow.Controllers
 {
     public class AdminController : Controller
     {
+        private void ShowDataTable(DataTable dt)
+        {
+            foreach (DataColumn column in dt.Columns)
+            {
+                Console.Write($"{column.ColumnName, 20}");
+            }
+            Console.WriteLine();
+            foreach (DataRow row in dt.Rows)
+            {
+                for(int i = 0; i < dt.Columns.Count; i++)
+                {
+                    Console.Write($"{row[i], 20}");
+                }
+                Console.WriteLine();
+            }
+        }
+        private void DataTable()
+        {
+            Database db = new Database();
+            db.Connection();
+            
+            // 
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.TableMappings.Add("Table", "v_Question");
+            da.SelectCommand = new SqlCommand("SELECT * FROM v_Question", db.Connection());
+            
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            
+            DataTable dt = ds.Tables["v_Question"];
+            ShowDataTable(dt);
+        } 
+
         // GET
         [LoginAuthenticationFilter]
         public ActionResult Index()
         {
             return RedirectToAction("dashboard");
         }
-        
+
         // GET: DashBoard
         [HttpGet]
         [LoginAuthenticationFilter]
         public ActionResult DashBoard()
         {
+            DataTable();
             return View();
         }
-        
+
         // GET: Login
         [HttpGet]
         public ActionResult Login()
         {
             return View("Login/Index");
         }
-        
+
         [HttpPost]
         public ActionResult Login(User user)
         {
@@ -61,7 +98,7 @@ namespace StackOverflow.Controllers
         {
             try
             {
-                BCP b = new BCP(@"localhost\SQLEXPRESS", "Forum");
+                BCP b = new BCP(@"localhost", "Forum");
                 string tableName = "QUESTIONS";
 
                 string tempFile = Path.GetTempFileName();
@@ -75,7 +112,6 @@ namespace StackOverflow.Controllers
 
                 string outputFileName = $"{tableName}_export.csv";
                 return File(fileBytes, "text/csv", outputFileName);
-
             }
             catch (Exception ex)
             {
