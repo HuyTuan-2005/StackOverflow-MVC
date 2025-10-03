6b22e29460daddf3ce1cc09d9d2f80e4b0078f7e
HuyTuan <HuyTuan.learn@gmail.com>
2025-10-04 06:03:24 +0700

    add helpers folder

---
+++ StackOverflow/Helpers/DateTimeHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace StackOverflow.Helpers
+{
+    public class DateTimeHelper
+    {
+        public static string ConvertDateToString(DateTime date)
+        {
+            TimeSpan ts = DateTime.Now - date;
+    
+            if (ts.TotalMilliseconds < 1000)
+            {
+                return "Vừa mới";
+            }
+            else if (ts.TotalSeconds < 60)
+            {
+                return (int)ts.TotalSeconds + " giây trước";
+            }
+            else if (ts.TotalMinutes < 60)
+            {
+                return (int)ts.TotalMinutes + " phút trước";
+            }
+            else if (ts.TotalHours < 24) 
+            {
+                return (int)ts.TotalHours + " giờ trước";
+            }
+            else if (ts.TotalDays < 7)
+            {
+                return (int)ts.TotalDays + " ngày trước";
+            }
+            else if (ts.TotalDays < 30)
+            {
+                return (int)(ts.TotalDays / 7) + " tuần trước";
+            }
+            else if (ts.TotalDays < 365)
+            {
+                // Tính tháng chính xác dựa trên DateTime
+                int months = ((DateTime.Now.Year - date.Year) * 12) + DateTime.Now.Month - date.Month;
+                if (DateTime.Now.Day < date.Day)
+                    months--;
+                return months + " tháng trước";
+            }
+            else
+            {
+                // Tính năm chính xác
+                int years = DateTime.Now.Year - date.Year;
+                if (DateTime.Now.Month < date.Month || 
+                    (DateTime.Now.Month == date.Month && DateTime.Now.Day < date.Day))
+                    years--;
+                return years + " năm trước";
+            }
+        }
+    }
+}
--- StackOverflow/Services/QuestionService.cs
+++ StackOverflow/Services/QuestionService.cs
@@ -1,4 +1,5 @@
-﻿using StackOverflow.ViewModels;
+﻿using System;
+using StackOverflow.ViewModels;
 using System.Collections.Generic;
 using System.Linq;
 using StackOverflow.Repositories;
